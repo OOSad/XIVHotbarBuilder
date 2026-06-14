ebe370dc89cb9b073e625d3328f809813a6aa3fc
OOSad <77910065+OOSad@users.noreply.github.com>
2026-06-14 18:35:01 -0300

    Delete XIVHotbarBuilder directory

--- XIVHotbarBuilder/find_cats.ps1
+++
@@ -1,8 +0,0 @@
-$content = Get-Content "items.lua"
-$cats = @{}
-foreach ($line in $content) {
-    if ($line -match 'category="([^"]+)"') {
-        $cats[$matches[1]] = 1
-    }
-}
-$cats.Keys | Out-File "categories.txt"
